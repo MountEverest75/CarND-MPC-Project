cf690d941a048a3277218dada28f9a2e0652350a
MountEverest <ravikiranmailid@gmail.com>
2017-06-29 14:06:48 -0700

    Chore:Draw reference and MPC trajectories

--- src/main.cpp
+++ src/main.cpp
@@ -137,9 +137,18 @@ int main() {
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory
+          //Display the MPC predicted trajectory (Green Line)
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
+
+          for(int i=2; i < vars.size(); i++) {
+              if(i%2 == 0) {
+                mpc_x_vals.push_back(vars[i]);
+              }
+              else {
+                mpc_y_vals.push_back(vars[i]);
+              }
+          }
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
@@ -147,9 +156,17 @@ int main() {
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
-          //Display the waypoints/reference line
+
+          //Display the waypoints/reference line (Yellow Line)
           vector<double> next_x_vals;
           vector<double> next_y_vals;
+
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for(int i=0; i < num_points; i++) {
+            next_x_vals.push_back(poly_inc*i);
+            next_y_vals.push_back(polyeval(coeffs, poly_inc*i));
+          }
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
